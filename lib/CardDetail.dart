e8e3c4c745c97a1a54bee1098417e03cf332638d
Tyler Baldwin <tjb9197@rit.edu>
2019-10-27 19:48:11 -0400

    adds anemls

--- lib/CardDetail.dart
+++ lib/CardDetail.dart
@@ -95,27 +95,27 @@ class CardDetailsState extends State<CardDetails> {
     switch (value) {
       case 0:
         placeDetails =
-        'Mussoorie, located around an hour from Derahdun in Uttarakhand, is a popular weekend destination for north Indians, as well as honeymooners. One of the reasons for Mussoories popularity is that it has a lot of facilities developed especially for tourists. Take a cable car to Gun Hill, enjoy a beautiful nature walk along Camels Back Road, have a picnic at Kempty Falls, or ride a horse up to Lal Tibba (the highest peak in Mussoorie). Mussoorie also offers a superb view of the Himalayas.';
+        "The cute dog is cute";
         break;
 
       case 1:
         placeDetails =
-        "Manali, in Himachal Pradesh, is one of the top adventure travel destinations in India.Manali, with its soothing backdrop of the Himalayas, offers a blend of tranquility and adventure that makes it one of northern India's most popular destinations. Although it's a popular place to go off on treks, you can do as little or as much as you want there. Located in the Kullu Valley of Himachal Pradesh, it's bordered by cool pine forest and the raging Beas River, which give it a special energy.";
+        "This is a baby cat. Very cute! See BuzzFeed for the 20 cutest cats!";
         break;
 
       case 2:
         placeDetails =
-        "Sikkim's capital, Gangtok, sits along a cloudy mountain ridge about 5,500 feet above sea level. Sikkim only became part of India in 1975. Before that, it was a small independent Buddhist kingdom with its own monarchy after the end of British rule. Gangtok is a popular base for travel throughout the state, particularly with trekkers. It's a well-organized and clean city with strict littering, traffic and tobacco laws. Attractions include monasteries, viewpoints, a cable car, and a zoo that houses rare animals rescued from traders and poachers.";
+        "This viral cat needs to lose weight. For more infomation, visit Reddit.";
         break;
 
       case 3:
         placeDetails =
-        "Darjeeling is also famous for its lush tea gardens. In addition, it's blessed with a stunning view of Mount Kanchenjunga, the world's third highest peak. Some of Darjeeling's most popular attractions include historic toy train, monasteries, botanical gardens, a zoo, and the Darjeeling-Rangeet Valley Passenger Ropeway (the longest aerial tramway in Asia). Darjeeling is a great place to walk around and explore the tea estates, villages, and markets. ";
+        "This is a cute basset hound.";
         break;
 
       case 4:
         placeDetails =
-        "The hill station of Nainital, in the Kumaon region of Uttarakhand, was a popular summer retreat for the British during the time they ruled India. It features the serene, emerald-colored Naini Lake and an action-filled strip called The Mall, lined with restaurants, shops, hotels, and markets. Enjoy one of the many forest walks, explore the surrounding area on horseback, or relax on a boat in the lake.";
+        "Because of climate change, all penguins have been extinct. There is the Linux pengun. It is the only pengun that surveived.";
         break;
     }
   }
