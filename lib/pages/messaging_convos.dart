8aa8c520f02e36ff2f1d5b0528c2f1046630b5ba
SWilson333 <sxw3165@rit.edu>
2019-10-27 18:49:04 -0400

    things

---
+++ lib/pages/messaging_convos.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:icebreaker/pages/login_page.dart';
+import 'package:multi_page_form/multi_page_form.dart';
+import 'package:icebreaker/globals.dart' as globals;
+import 'package:icebreaker/profile.dart' as profile;
+
+class MessagingConvos extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+  }
+
+
+
+}
